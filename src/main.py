362b6bba66fb217e1379a0e803976999f7e91f7b
Jas Legion <jarangos@outlook.com>
2025-04-10 22:59:35 -0500

    configuracion cargada

--- src/main.py
+++ src/main.py
@@ -4,24 +4,25 @@ from cryptography.fernet import Fernet
 from outlook_reader import * #obtener_transacciones
 from excel_writer import * #guardar_transacciones_excel
 from notion_conn import * #sincronizar_notion
-print("Starting main.py")
-# # Cargar variables de entorno
-# def cargar_config():
-#     load_dotenv()
-#     with open("secret.key", "rb") as f:
-#         key = f.read()
-#     fernet = Fernet(key)
-#     return {
-#         'EMAIL': os.getenv("EMAIL"),
-#         'PASSWORD': fernet.decrypt(os.getenv("PASSWORD_ENCRYPTED").encode()).decode(),
-#         'EXCEL_PATH': os.getenv("EXCEL_PATH"),
-#         'HOJA_EXCEL': os.getenv("HOJA_EXCEL"),
-#         'NOTION_TOKEN': fernet.decrypt(os.getenv("NOTION_TOKEN_ENCRYPTED").encode()).decode(),
-#         'NOTION_DATABASE_ID': os.getenv("NOTION_DATABASE_ID")
-#     }
 
-# def main():
-#     config = cargar_config()
+# Cargar variables de entorno
+def cargar_config():
+    load_dotenv()
+    with open("secret.key", "rb") as f:
+        key = f.read()
+    fernet = Fernet(key)
+    return {
+        'EMAIL': os.getenv("EMAIL"),
+        'PASSWORD': fernet.decrypt(os.getenv("PASSWORD_MAIL").encode()).decode(),
+        'EXCEL_PATH': os.getenv("EXCEL_PATH"),
+        'HOJA_EXCEL': os.getenv("HOJA_EXCEL"),
+        'NOTION_TOKEN': fernet.decrypt(os.getenv("NOTION_TOKEN").encode()).decode(),
+        'NOTION_DATABASE_ID': os.getenv("NOTION_DATABASE_ID")
+    }
+
+def main():
+    config = cargar_config()
+    print("🔑 Configuración cargada.")
 
 #     transacciones = obtener_transacciones(config['EMAIL'], config['PASSWORD'])
 
@@ -31,5 +32,5 @@ print("Starting main.py")
 #     else:
 #         print("📭 No se encontraron nuevas transacciones.")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
