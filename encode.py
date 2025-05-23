dc8dfcdd9911f1b2e7791dfd941fb04ad4be6d24
Jas_Legion <jarangos@outlook.com>
2025-05-23 18:46:38 -0500

    Cambios en codificador y decodificador

--- encode.py
+++ encode.py
@@ -1,14 +1,27 @@
-from cryptography.fernet import Fernet #pip install cryptography
+from cryptography.fernet import Fernet #pip install 
+from tkinter import filedialog
 #este codigo genera una clave de encriptacion en un archivo llamado secret.key, solo se debe ejecutar una vez.
 def gen_key():
+    output_path = filedialog.asksaveasfilename(
+        title="Guardar archivo PDF combinado",
+        defaultextension=".key",
+        filetypes=[("Claves de Seguridad", "*.key")]  # Opcional: Filtra por tipo de archivo
+    )
     key = Fernet.generate_key()
-    with open("secret.key", "wb") as key_file:
+    with open(output_path, "wb") as key_file:
         key_file.write(key)
-    print("🔑 Clave generada y guardada en 'secret.key'")
+    print(f"🔑 Clave generada y guardada en '{output_path}'")
 
 #este codigo encripta una contraseña y la imprime en la consola, se debe ejecutar cada vez que se quiera encriptar una nueva contraseña.
 def enc_data(value):
-    with open("secret.key", "rb") as key_file:
+    input_path = filedialog.askopenfilename(
+        title="Seleccione la llave de encriptación",
+        filetypes=[("Claves de Seguridad", "*.key")]
+    )
+    if not input_path:
+        return
+
+    with open(input_path, "rb") as key_file:
         key = key_file.read()
 
     fernet = Fernet(key)
@@ -19,10 +32,36 @@ def enc_data(value):
     print("Contraseña encriptada:")
     print(encrypted.decode())
 
+def dec_data(value):
+    input_path = filedialog.askopenfilename(
+        title="Seleccione la llave de encriptación",
+        filetypes=[("Claves de Seguridad", "*.key")]
+    )
+    if not input_path:
+        return
+
+    with open(input_path, "rb") as key_file:
+        key = key_file.read()
+
+    fernet = Fernet(key)
+
+    password = value
+    decrypted = fernet.decrypt(password.encode())
+
+    print("Contraseña desencriptada:")
+    print(decrypted.decode())
+
 if __name__ == "__main__":
-    value=input("Introduce la contraseña a encriptar: ")
-    if value=="":
+    value=input("seleccione una opción:\n1. Desencriptar\n2. Encriptar\n3. Generar clave\n")
+    # value=input("Contraseña a encriptar o desencriptar:\n")
+    if value=="3":
         print("Gen Key")
         gen_key()
-    else:
+    elif value=="1":
+        value=input("Contraseña a desencriptar:\n")
+        dec_data(value)
+    elif value=="2":
+        value=input("Contraseña a encriptar:\n")
         enc_data(value)
+    else:
+        print("Opción no válida")
